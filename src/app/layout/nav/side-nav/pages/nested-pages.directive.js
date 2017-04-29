070a1e9eb72bb076e2e7b3581f4d116a10ffc0f6
Alexandra <alexandra@webcrumbz.co>
2017-04-29 12:12:06 +0300

    refactor: refactor pages submenu
    remove nested functionality, add api calls in the directive controller

--- src/app/layout/nav/side-nav/pages/nested-pages.directive.js
+++ src/app/layout/nav/side-nav/pages/nested-pages.directive.js
@@ -2,14 +2,17 @@ angular
   .module('appticles.nav')
   .directive('appticlesNestedPages', AppticlesNestedPages);
 
-AppticlesNestedPages.$inject = ['$log', '$state', '$ionicScrollDelegate', '$ionicSideMenuDelegate'];
-
-function AppticlesNestedPages($log, $state, $ionicScrollDelegate, $ionicSideMenuDelegate) {
+/**
+ * @ngdoc directive
+ * @name appticles.layout.pages.AppticlesNestedPages
+ *
+ * @description Render a menu with nested pages.
+ */
+function AppticlesNestedPages() {
   return {
     restrict: 'AE',
     scope: {
-      pages: '=',
-      directiveApi: '='
+      state: '=',
     },
     templateUrl: 'app/layout/nav/side-nav/pages/nested-pages.template.html',
     controller: NestedPagesController,
@@ -18,81 +21,206 @@ function AppticlesNestedPages($log, $state, $ionicScrollDelegate, $ionicSideMenu
   };
 }
 
-
+/**
+ * @ngdoc controller
+ * @name appticles.layout.pages.NestedPagesController
+ *
+ * @description Controller for the directive that loads the pages submenu.
+ *
+ * @todo Add unit tests
+ */
 class NestedPagesController {
-  constructor($log, $document, configuration, $ionicScrollDelegate, $ionicSideMenuDelegate, $state, $filter) {
-    this.openContent = openContent;
-    this.goBackTo = goBackTo;
+  constructor(
+    AppticlesAPI,
+    AppticlesValidation,
+    configuration,
+    $q,
+    $ionicScrollDelegate,
+    $ionicSideMenuDelegate,
+    $state,
+    $filter,
+    $log) {
 
-    let initialPagelist = this.pages;
-    this.currentPages = this.buildNestedTree(this.pages, 0);
+    // list of all pages read from the API
+    this.allPages = [];
 
-    let previousBackButtonTitle = '';
-    this.backButtonTitle = $filter('translate')('LINKS.GO_TO');
+    // list of pages displayed in the current view
+    this.pages = [];
 
-    const openChildPages = (parentPageId) => {
-      // prevent scrolling issues by scrolling to top before updating the pageList
-      $ionicScrollDelegate.scrollTop();
-      previousBackButtonTitle = this.backButtonTitle;
+    this.contentLoaded = false;
 
-      let parentPage = this.currentPages.filter((page) => page.id === parentPageId);
-      this.backButtonTitle = parentPage[0].title;
-      this.currentPages = parentPage[0].children;
-      this.currentParentId = this.currentPages[0].parent_id;
+    // the top level parent of the submenu
+    this.currentParent = {
+      'id': 0,
+      'title': $filter('translate')('LINKS.GO_TO')
     };
+
     this.openChildPages = openChildPages;
+    this.goBack = goBack;
+    this.openContent = openContent;
 
-    function goBackTo(parentPageId = 0) {
-      if (parentPageId === 0) {
-        this.directiveApi.isRoot = true;
-        this.directiveApi.pagesVisible = false;
-        this.shouldAnimate = false;
+    /**
+     * @ngdoc function
+     * @name appticles.layout.pages.NestedPagesController#validateData
+     * @description Internal method, validate pages.
+     *
+     * @param {Promise} A promise object with the pages, returned by the API.
+     *
+     * @return {Promise} A promise object with an array of validated pages.
+     */
+    const validateData = (result) => {
+
+      let validPages = AppticlesValidation.validatePages(result);
+
+      if (angular.isDefined(validPages.error)) {
+        return $q.reject('error fetching pages');
       }
-      else {
-        let parentPageItem = initialPagelist.filter((page) => page.id === parentPageId)[0];
-        let listAtParentPageLevel = initialPagelist.filter((page) => page.parent_id === parentPageItem.parent_id);
 
-        this.currentPages = listAtParentPageLevel;
-        this.currentParentId = parentPageItem.parent_id;
-        let parentOfIncomingList = initialPagelist.filter((page) => page.id === this.currentParentId)[0];
-        this.backButtonTitle = this.currentParentId === 0 ? $filter('translate')('LINKS.GO_TO') : parentOfIncomingList.title;
+      return $q.when(validPages);
+    };
+
+    /**
+     * @ngdoc function
+     * @name appticles.layout.pages.NestedPagesController#populateData
+     * @description Internal method, bind results to the controller properties.
+     *
+     * @param {Promise} A promise object with an array of pages.
+     */
+    const populateData = (result) => {
+
+      if (result.length > 0) {
+
+        this.allPages = this.checkChildren(result);
+        this.pages = this.getChildPagesByParentId(0);
+      }
+    };
+
+    /**
+     * @ngdoc function
+     * @name appticles.layout.pages.NestedPagesController#openChildPages
+     * @description Open submenu when a parent page is clicked.
+     *
+     * @param {Object} The page that was clicked.
+     */
+    function openChildPages(page) {
+
+      // prevent scrolling issues by scrolling to top before updating the page list
+      $ionicScrollDelegate.scrollTop();
+
+      this.currentParent = page;
+      this.pages = this.getChildPagesByParentId(page.id);
+    };
+
+    /**
+     * @ngdoc function
+     * @name appticles.layout.pages.NestedPagesController#goBack
+     * @description Go up one level in the page hierarchy.
+     */
+    function goBack() {
+
+      if (this.currentParent.id === 0) {
+        // exit the pages menu
+        this.state = 'root';
+      } else {
+
+        let page = this.getPageById(this.currentParent.id);
+
+        if (page !== false) {
+
+          if (page.parent_id == 0){
+
+            this.currentParent = {
+              'id': 0,
+              'title': $filter('translate')('LINKS.GO_TO')
+            };
+
+          } else {
+
+            let parentPage = this.getPageById(page.parent_id);
+
+            if (parentPage !== false) {
+              this.currentParent = parentPage;
+            }
+          }
+
+          this.pages = this.getChildPagesByParentId(this.currentParent.id);
+        }
       }
     }
 
+    /**
+     * @ngdoc function
+     * @name appticles.layout.pages.NestedPagesController#openContent
+     * @description Open the page's details or submenu if the page doesn't have any content.
+     *
+     * @param {Object} The page that was clicked.
+     */
     function openContent(page) {
-      if (!page.has_content && page.children.length > 0) {
-        openChildPages(page.id);
+      if (!page.has_content && page.has_children) {
+        this.openChildPages(page);
         return;
       }
 
       $ionicSideMenuDelegate.toggleRight(false);
       $state.go('app.nav.page-details', { pageId: page.id });
     }
+
+    AppticlesAPI.findPages()
+      .then(validateData)
+      .then(populateData)
+      .finally(() => {
+        this.contentLoaded = true;
+      })
+      .catch($log.error);
   }
 
   /**
-   * Build tree with the pages/categories.
-   * Parent pages/categories will contain a list with their children.
+   * @ngdoc function
+   * @name appticles.layout.pages.NestedPagesController#getPageById
+   * @description Search a page by its id in the list of pages.
+   *
+   * @param {Number} The id of the page
    */
-  buildNestedTree(items, parent) {
+  getPageById(id) {
+
+    let filteredPages = this.allPages.filter((page) => page.id === id);
+
+    if (filteredPages.length > 0){
+      return filteredPages[0];
+    }
+
+    return false;
+  }
+
+  /**
+   * @ngdoc function
+   * @name appticles.layout.pages.NestedPagesController#getChildPagesByParentId
+   * @description Search pages by their parent id in the list of pages.
+   *
+   * @param {Number} The id of the parent
+   */
+  getChildPagesByParentId(parentId) {
+    return this.allPages.filter((page) => page.parent_id === parentId);
+  }
+
+  /**
+   * @ngdoc function
+   * @name appticles.layout.pages.NestedPagesController#checkChildren
+   * @description Attach a has_children property to the list of pages.
+   *
+   * @param {Array} A list of pages objects
+   */
+  checkChildren(items) {
 
     return items
-      .filter((item) => item.parent_id == parent)
       .map((item) => {
-        if (item.id == 0) return item;
 
-        let children = this.buildNestedTree(items, item.id);
-
-        if (children.length > 0) {
-          item.children = children;
-
-          // Set active = 0 for parent items, to hide their submenus
-          item.active = 0;
-        }
+        let children = items.filter((child) => child.parent_id == item.id);
+        item.has_children = children.length > 0;
 
         return item;
       });
   }
 };
 
-NestedPagesController.$inject = ['$log', '$document', 'configuration', '$ionicScrollDelegate', '$ionicSideMenuDelegate', '$state', '$filter'];
+NestedPagesController.$inject = ['AppticlesAPI', 'AppticlesValidation', 'configuration', '$q', '$ionicScrollDelegate', '$ionicSideMenuDelegate', '$state', '$filter', '$log'];
