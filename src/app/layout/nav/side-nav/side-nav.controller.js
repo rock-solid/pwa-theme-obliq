6456f340bcd8a3967854419cbd61eae661003fc4
Alexandra <alexandra@webcrumbz.co>
2017-04-29 12:10:15 +0300

    refactor: refactor side navigation
    load a single category and page for displaying the menu options
    
    closes #24

--- src/app/layout/nav/side-nav/side-nav.controller.js
+++ src/app/layout/nav/side-nav/side-nav.controller.js
@@ -1,37 +1,107 @@
+/**
+ * @ngdoc controller
+ * @name appticles.layout.SideNavController
+ *
+ * @description Responsible for loading a side menu and checking if we have pages and categories.
+ * @todo Add unit tests
+ */
 class SideNav {
-  constructor(AppticlesAPI, AppticlesValidation, $q, $log, $window, $translate, configuration) {
-    this.allPages = [];
-    this.allCategories = [];
+  constructor(
+    AppticlesAPI,
+    AppticlesValidation,
+    configuration,
+    $q,
+    $window,
+    $translate,
+    $log ) {
 
-    this.websiteUrl = configuration.websiteUrl;
+    this.state = 'root';
+
+    this.hasCategories = false;
+    this.hasPages = false;
+
+    this.contentLoaded = false;
+
+    this.websiteUrl = configuration.websiteUrl || '';
+
+    this.openCategories = openCategories;
+    this.openPages = openPages;
     this.openDesktopWebsite = openDesktopWebsite;
 
-    this.pageContentLoaded = false;
-    this.categoryContentLoaded = false;
+    /**
+     * @ngdoc function
+     * @name appticles.layout.SideNavController#getCategoriesPages
+     * @description Internal method, call API to load a single category and a page.
+     *
+     * @return {Promise} A promise object which resolves to an array with a category and a page.
+     */
+    const getCategoriesPages = () => {
 
-    this.loadPages = loadPages;
-    this.loadCategories = loadCategories;
+      let promises = {
+        categories: AppticlesAPI.findCategories({ page: 1, rows: 1, withArticles: 0 }),
+        pages: AppticlesAPI.findPages({ page: 1, rows: 1 })
+      };
 
-    this.hasCategories = true;
-    this.hasPages = true;
-
-    this.directiveApi = {
-      isRoot: true,
-      pagesVisible: false,
-      categoriesVisible: false,
-      currentPaginationForCategories: 1,
-      moreCategoriesAvailable: true
+      return $q.all(promises);
     };
 
-    this.loadPages = loadPages;
+    /**
+     * @ngdoc function
+     * @name appticles.layout.SideNavController#validateData
+     * @description Internal method, validate the category and page.
+     *
+     * @param {Promise} A promise object with the category and page, returned by the API.
+     *
+     * @return {Promise} A promise object with a validated category and page or a reject promise.
+     */
+    const validateData = (result) => {
 
+      let validCategories = AppticlesValidation.validateCategories(result.categories);
+      let validPages = AppticlesValidation.validatePages(result.pages);
+
+      if (validPages.error || validCategories.error) {
+        return $q.reject('error fetching categories or pages');
+      }
+
+      let promises = {
+        categories: validCategories,
+        pages: validPages
+      };
+
+      return $q.all(promises);
+    };
+
+    /**
+     * @ngdoc function
+     * @name appticles.layout.SideNavController#populateData
+     * @description Internal method, bind results to the controller properties.
+     *
+     * @param {Promise} A promise object with a validated category and page.
+     */
+    const populateData = (result) => {
+
+      this.hasCategories = result.categories.length > 0;
+      this.hasPages = result.pages.length > 0;
+    };
+
+    /**
+     * @ngdoc function
+     * @name appticles.layout.SideNavController#confirmNavigation
+     * @description Internal method, redirect user to the desktop version.
+     *
+     */
     const confirmNavigation = (message) => {
-
       if (confirm(message)) {
         $window.open(this.websiteUrl, '_self', 'location=yes');
       }
     };
 
+    /**
+     * @ngdoc function
+     * @name appticles.layout.SideNavController#openDesktopWebsite
+     * @description Ask user if he wants to navigate to the desktop version.
+     *
+     */
     function openDesktopWebsite() {
       if (this.websiteUrl) {
         $translate('TEXTS.WEBSITE_CONFIRM')
@@ -39,111 +109,37 @@ class SideNav {
       }
     };
 
-    const validatePages = (result) => {
-
-      let validPages = AppticlesValidation.validatePages(result);
-
-      if (angular.isDefined(validPages.error)) {
-        return $q.reject('error fetching page list');
-      }
-
-      return $q.when(validPages);
-    };
-
-    const populateSideNav = (result) => {
-      // sort pages
-      if (angular.isDefined(result)) {
-        let pages = result.sort((a, b) => { return a.order - b.order; });
-        this.allPages = pages;
-      }
-    };
-
-    function loadPages() {
-
-      // if we haven't already loaded the pages
-      if(!this.pageContentLoaded) {
-        AppticlesAPI
-          .findPages()
-          .then(validatePages)
-          .then(populateSideNav)
-          .then(() => {
-            this.pageContentLoaded = true;
-            this.directiveApi.isRoot = false;
-            this.directiveApi.pagesVisible = true;
-          })
-          .catch($log.error);
-      }
-      else {
-        this.pageContentLoaded = true;
-        this.directiveApi.isRoot = false;
-        this.directiveApi.pagesVisible = true;
-      }
+    /**
+     * @ngdoc function
+     * @name appticles.layout.SideNavController#openCategories
+     * @description Open the categories submenu.
+     *
+     */
+    function openCategories() {
+      this.state = 'categories';
     }
 
-    function checkHasPages() {
-      let hasPages = AppticlesAPI
-        .findPages({ page: 1, rows: 1 })
-        .then((res) => res.data.pages && res.data.pages.length > 0 ? true : false);
-
-      return $q.when(hasPages);
-    };
-    checkHasPages().then(res => this.hasPages = res);
-
-
-    function checkHasCategories() {
-      let hasCategories = AppticlesAPI
-        .findCategories({ withArticles: 0, page: 1, rows: 1 })
-        .then((res) => res.data.categories && res.data.categories.length > 0 ? true : false);
-
-      return $q.when(hasCategories);
-    };
-    checkHasCategories().then(res => this.hasCategories = res);
-
-
-    const validateCategories = (result) => {
-
-      let validCategories = AppticlesValidation.validateCategories(result);
-
-      if (angular.isDefined(validCategories.error)) {
-        return $q.reject('error fetching category list');
-      }
-
-      return $q.when(validCategories);
-    };
-
-    const populateCategoryList = (result) => {
-      // sort pages
-      if (angular.isDefined(result)) {
-        let categories = result.sort((a, b) => { return a.order - b.order; }).filter(category => category.id !== 0);
-        this.allCategories = categories;
-      }
-    };
-
-    function loadCategories() {
-      const countOfCategories = 10;
-
-      if(!this.categoryContentLoaded) {
-        AppticlesAPI
-          .findCategories({ withArticles: 1, limit: 1, page: this.directiveApi.currentPaginationForCategories, rows: countOfCategories })
-          .then(validateCategories)
-          .then(populateCategoryList)
-          .then(() => {
-            this.categoryContentLoaded = true;
-            this.directiveApi.isRoot = false;
-            this.directiveApi.categoriesVisible = true;
-          })
-          .catch($log.error);
-      }
-      else {
-        this.categoryContentLoaded = true;
-        this.directiveApi.isRoot = false;
-        this.directiveApi.categoriesVisible = true;
-      }
+    /**
+     * @ngdoc function
+     * @name appticles.layout.SideNavController#openPages
+     * @description Open the pages submenu.
+     *
+     */
+    function openPages() {
+      this.state = 'pages';
     }
+
+    getCategoriesPages()
+      .then(validateData)
+      .then(populateData)
+      .finally(() => {
+        this.contentLoaded = true;
+      })
+      .catch($log.error);
   }
 }
 
-SideNav.$inject = ['AppticlesAPI', 'AppticlesValidation', '$q', '$log', '$window', '$translate', 'configuration'];
+SideNav.$inject = ['AppticlesAPI', 'AppticlesValidation', 'configuration', '$q', '$window', '$translate', '$log'];
 
 angular.module('appticles.nav')
   .controller('SideNavController', SideNav);
