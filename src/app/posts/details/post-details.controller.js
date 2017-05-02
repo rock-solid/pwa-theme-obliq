4a472a38981c4af5675db4dc5abd54a4f18cece1
Alexandra <alexandra@webcrumbz.co>
2017-05-02 18:01:25 +0300

    refactor: rename controller properties, reorganize code

--- src/app/posts/details/post-details.controller.js
+++ src/app/posts/details/post-details.controller.js
@@ -1,3 +1,9 @@
+/**
+ * @ngdoc controller
+ * @name appticles.posts.PostDetails
+ *
+ * @description Responsible for displaying a post's details.
+ */
 class PostDetails {
   constructor(AppticlesAPI,
     AppticlesValidation,
@@ -8,38 +14,18 @@ class PostDetails {
     $window,
     $ionicScrollDelegate,
     $scope,
-    $log,
     $q,
-    $ionicLoading) {
-
-    this.itemIsShown = true;
-    this.handleOnScroll = function($event) {
-      this.deltaY = (this.lastScrollY) ? $ionicScrollDelegate.getScrollPosition().top - this.lastScrollY : 0;
-      // scroll down
-      if (this.deltaY > 0 && this.itemIsShown == true) {
-        this.itemIsShown = false;
-        $scope.$applyAsync();
-      }
-      // scroll up
-      else if (this.deltaY < 0 && this.itemIsShown == false) {
-        this.itemIsShown = true;
-        $scope.$applyAsync();
-      }
-      this.lastScrollY = $ionicScrollDelegate.getScrollPosition().top;
-    };
-
-    this.handleOnTap = function() {
-      this.itemIsShown = !this.itemIsShown ? true : this.itemIsShown;
-      $scope.$applyAsync();
-    };
+    $ionicLoading,
+    $log) {
 
     this.contentLoaded = false;
-    this.postDetails = '';
-    this.category = '';
+    this.post = null;
+    this.category = null;
+
     this.fromLatest = (angular.isDefined($stateParams.latest) && Number($stateParams.latest) === 1) ||
       (angular.isDefined($stateParams.categorySlugId) && Number($stateParams.categorySlugId) === 0);
-    this.hasSocialNetworks = this.checkHasSocialNetwork(configuration.socialMedia);
-    this.goBack = goBack;
+
+    this.hasSocialNetworks = this.checkHasSocialNetworks(configuration.socialMedia);
     this.postCover = configuration.defaultCover;
 
     this.directiveApi = {
@@ -49,32 +35,43 @@ class PostDetails {
     const postId = $stateParams.postId;
     let categorySlugId = $stateParams.categorySlugId || undefined;
 
-    const showLoader = () => {
-      $ionicLoading.show();
-    };
+    this.buttonsVisible = true;
+    this.handleOnScroll = handleOnScroll;
+    this.handleOnTap = handleOnTap;
+    this.goBack = goBack;
 
-    const hideLoader = () => {
-      $ionicLoading.hide();
-      this.contentLoaded = true;
-    };
-
+    /**
+     * @ngdoc function
+     * @name appticles.posts.PostDetails#validatePost
+     * @description Internal method, validate post.
+     *
+     * @param {Promise} A promise with a post object.
+     *
+     * @return {Promise} A promise with a validated post object.
+     */
     const validatePost = (result) => {
 
-      let validPostDetails = AppticlesValidation.validateOnePosts(result);
+      let validPost = AppticlesValidation.validateOnePosts(result);
 
-      if (validPostDetails.error) {
+      if (validPost.error) {
         $state.go('app.nav.latest');
         return $q.reject('error fetching post details');
       }
 
-      this.postDetails = validPostDetails;
-      let commentsEnabled = this.postDetails['comment_status'] == 'open' || (this.postDetails['comment_status'] == 'closed' && this.postDetails['no_comments'] > 0);
-      this.directiveApi.hasComments = commentsEnabled ? true : false;
+      this.post = validPost;
 
-      // @todo when do we need to return a promise?
-      return $q.when({ 'categories': validPostDetails.categories });
+      return $q.when({ 'categories': validPost.categories });
     };
 
+    /**
+     * @ngdoc function
+     * @name appticles.posts.PostDetails#getCategory
+     * @description Internal method, make request to read a category.
+     *
+     * @param {Promise} A promise with a list of categories ids.
+     *
+     * @return {Promise} A promise for retrieving a category.
+     */
     const getCategory = (result) => {
 
       // if we don't have a category defined id, get one from the categories list of the post
@@ -85,12 +82,21 @@ class PostDetails {
       return AppticlesAPI.findOneCategories({ categoryId: categorySlugId });
     };
 
+    /**
+     * @ngdoc function
+     * @name appticles.posts.PostDetails#validateCategory
+     * @description Internal method, validate category.
+     *
+     * @param {Promise} A promise with a category object.
+     *
+     * @return {Promise} A promise with a validated category object.
+     */
     const validateCategory = (result) => {
 
       let validCategory = AppticlesValidation.validateOneCategories(result);
 
       // check if the category is valid and includes the current post
-      if (validCategory.error || !this.postDetails.categories.includes(validCategory.id)) {
+      if (validCategory.error || !this.post.categories.includes(validCategory.id)) {
         $state.go('app.nav.latest');
         return $q.reject('error fetching category details');
       }
@@ -98,65 +104,115 @@ class PostDetails {
       this.category = validCategory;
     };
 
-    const populatePostDetails = (result) => {
+    /**
+     * @ngdoc function
+     * @name appticles.pages.PageDetails#populateData
+     * @description Internal method, bind results to the controller properties.
+     */
+    const populateData = () => {
 
       this.directiveApi.props = {
-        'id': this.postDetails.id,
-        'title': $window.encodeURIComponent(this.postDetails.title),
-        'link': this.postDetails.link,
-        'no_comments': this.postDetails.no_comments,
-        'comment_status': this.postDetails['comment_status'] || 'disabled',
-        'require_name_email': this.postDetails['require_name_email'] || 0,
+        'id': this.post.id,
+        'title': $window.encodeURIComponent(this.post.title),
+        'link': this.post.link,
+        'has_comments': this.post.comment_status == 'open' || (this.post.comment_status == 'closed' && this.post.no_comments > 0),
+        'no_comments': this.post.no_comments,
+        'comment_status': this.post.comment_status || 'disabled',
+        'require_name_email': this.post.require_name_email || 0,
         'from_latest': Number(this.fromLatest),
         'category_id': this.category.id
       };
 
-      if (this.postDetails.link) {
-        AppticlesCanonical.set(this.postDetails.link);
+      if (this.post.link) {
+        AppticlesCanonical.set(this.post.link);
       }
     };
 
+    /**
+     * @ngdoc function
+     * @name appticles.posts.PostDetails#goBack
+     * @description Go back to a category or latest carousel.
+     */
     function goBack() {
       if (this.fromLatest) {
         return $state.go('app.nav.latest');
       }
 
       return $state.go('app.nav.category', { categorySlug: this.category.name_slug, categoryId: this.category.id });
-
     };
 
-    showLoader();
+    /**
+     * @ngdoc function
+     * @name appticles.posts.PostDetails#handleOnScroll
+     * @description Scroll event listener, display / hide back and social buttons.
+     *
+     * @param {Object} The scroll event
+     */
+    function handleOnScroll($event) {
+      this.deltaY = (this.lastScrollY) ? $ionicScrollDelegate.getScrollPosition().top - this.lastScrollY : 0;
+      // scroll down
+      if (this.deltaY > 0 && this.buttonsVisible == true) {
+        this.buttonsVisible = false;
+        $scope.$applyAsync();
+      }
+      // scroll up
+      else if (this.deltaY < 0 && this.buttonsVisible == false) {
+        this.buttonsVisible = true;
+        $scope.$applyAsync();
+      }
+      this.lastScrollY = $ionicScrollDelegate.getScrollPosition().top;
+    };
+
+    /**
+     * @ngdoc function
+     * @name appticles.posts.PostDetails#handleOnTap
+     * @description Tap event listener, display back and social buttons.
+     */
+    function handleOnTap() {
+      this.buttonsVisible = !this.buttonsVisible ? true : this.buttonsVisible;
+      $scope.$applyAsync();
+    };
+
+    $ionicLoading.show();
 
     AppticlesAPI.findOnePosts({ articleId: postId })
       .then(validatePost)
       .then(getCategory)
       .then(validateCategory)
-      .then(populatePostDetails)
-      .finally(hideLoader)
+      .then(populateData)
+      .finally(() => {
+        $ionicLoading.hide();
+        this.contentLoaded = true;
+      })
       .catch($log.error);
   }
 
-  checkHasSocialNetwork(socialNetworks) {
+  /**
+   * @ngdoc function
+   * @name appticles.posts.PostDetails#checkHasSocialNetworks
+   * @description Check if social sharing is enabled.
+   *
+   * @param {Object} The configuration.socialMedia object.
+   */
+  checkHasSocialNetworks(socialNetworks) {
+
     let disabledSocialNetworks = 0;
+
     if (!(angular.isObject(socialNetworks) && !angular.isArray(socialNetworks))) {
       return false;
     }
 
     Object.keys(socialNetworks).forEach((key) => {
-      if (socialNetworks[key] === false)
+      if (socialNetworks[key] === false) {
         disabledSocialNetworks++;
+      }
     });
 
-    if (disabledSocialNetworks === Object.keys(socialNetworks).length) {
-      return false;
-    }
-    else {
-      return true;
-    }
+    return disabledSocialNetworks < Object.keys(socialNetworks).length;
   }
 };
 
-PostDetails.$inject = ['AppticlesAPI', 'AppticlesValidation', 'AppticlesCanonical', 'configuration', '$stateParams', '$state', '$window', '$ionicScrollDelegate', '$scope', '$log', '$q', '$ionicLoading'];
+PostDetails.$inject = ['AppticlesAPI', 'AppticlesValidation', 'AppticlesCanonical', 'configuration', '$stateParams', '$state', '$window', '$ionicScrollDelegate', '$scope', '$q', '$ionicLoading', '$log'];
 
 angular.module('appticles.posts')
   .controller('PostDetailsController', PostDetails);
